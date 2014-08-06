5efa50669b0ea071b9daae9cc7cefe0a3dbaa060
jenmccarthy <jennifernward@gmail.com>
2014-08-06 10:00:50 -0700

    create a place for all tasks to be held

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -5,4 +5,24 @@ class Task
     @name = attributes['name']
     @list_id = attributes['list_id']
   end
+
+  def self.all
+    results = DB.exec("SELECT * FROM tasks;")
+    tasks = []
+    results.each do |result|
+      name = result['name']
+      list_id = result['list_id'].to_i
+      tasks << Task.new({'name' => name, 'list_id' => list_id})
+    end
+    tasks
+  end
+
+  def save
+    DB.exec("INSERT INTO tasks (name, list_id) VALUES ('#{@name}', #{@list_id});")
+  end
+
+  def ==(test)
+    self.name == test.name
+  end
+
 end
--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -1,9 +1,41 @@
 require 'rspec'
+require 'pg'
 require 'tasks'
 
+DB = PG.connect({:dbname => 'to_do1_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM tasks *;")
+  end
+end
+
 describe Task do
-  it "will initialize with a hash the tasks information" do
+  it 'will initialize with a hash the tasks information' do
     test_task = Task.new({'name' => 'wash the windows', 'list_id' => 1})
     expect(test_task).to be_an_instance_of Task
   end
+
+  it 'tells you its name and list_id' do
+    test_task = Task.new({'name' => 'wash the windows', 'list_id' => 1})
+    expect(test_task.name).to eq 'wash the windows'
+    expect(test_task.list_id). to eq 1
+  end
+
+  it 'will create a place to hold all tasks' do
+    expect(Task.all).to eq []
+  end
+
+  it 'will save all tasks to our class' do
+    test_task = Task.new({'name' => 'wash the windows', 'list_id' => 1})
+    test_task.save
+    expect(Task.all).to eq [test_task]
+  end
+
+  it 'will know that the objects are the same if they have the same name' do
+    test_task = Task.new({'name' => 'wash the windows', 'list_id' => 1})
+    another_test_task = Task.new({'name' => 'wash the windows', 'list_id' => 1})
+    expect(test_task).to eq another_test_task
+  end
+
 end
